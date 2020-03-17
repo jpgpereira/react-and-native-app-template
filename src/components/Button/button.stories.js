b292b906663a2064ee422dfd04a54703fe5c1a28
João Pereira <joao.pereira@comon.pt>
2020-03-17 12:43:27 +0000

    added example storybook stories for button and container

---
+++ src/components/Button/button.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const NormalButton = () => <Button text="CTA" onPress={() => {}} />;
---
+++ src/components/Container/container.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Container from './';
+
+export default {
+  title: 'Container',
+  component: Container,
+};
+
+export const SimpleContainer = () => <Container />;
