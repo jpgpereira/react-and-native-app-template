bb76ce025102fdeab28b8ebfb17b9d0853a3daa3
João Pereira <joao.pereira@comon.pt>
2020-03-21 19:36:18 +0000

    lint fix

--- src/components/Container/index.native.ts
+++ src/components/Container/index.native.ts
@@ -2,14 +2,7 @@ import styled from '@emotion/native';
 import {t} from 'react-native-tailwindcss';
 
 const Container = styled.View`
-  ${[
-    t.flex,
-    t.flex1,
-    t.flexCol,
-    t.justifyCenter,
-    t.itemsCenter,
-    t.hFull,
-  ]}
+  ${[t.flex, t.flex1, t.flexCol, t.justifyCenter, t.itemsCenter, t.hFull]}
 `;
 
 export default Container;
