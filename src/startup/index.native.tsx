8bf89697732f72fc7987e542afaec03b3a20b046
João Pereira <joao.pereira@comon.pt>
2020-03-21 11:16:55 +0000

    remove containers folder for now

--- src/startup/index.native.tsx
+++ src/startup/index.native.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import store, {persistor} from '../services/redux';
+import Router from '../navigation';
 import '../services/localization';
-import App from '../containers/App';
 import {name as appName} from '../../app.json';
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router />
+    </PersistGate>
+  </Provider>
+);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/startup/index.tsx
+++ src/startup/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../containers/App';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import store, {persistor} from '../services/redux';
+import Router from '../navigation';
 import 'normalize.css';
 import '../services/localization';
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router />
+    </PersistGate>
+  </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
